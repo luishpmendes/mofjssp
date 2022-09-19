d4483d1f633aa88b1157f4e8a6a1204401c633f9
Luis H. P. Mendes <luishpmendes@gmail.com>
2022-09-18 21:25:24 -0300

    Bugfix
    Signed-off-by: Luis H. P. Mendes <luishpmendes@gmail.com>

--- src/instance/instance.cpp
+++ src/instance/instance.cpp
@@ -104,7 +104,7 @@ Instance Instance::operator = (const Instance & instance) {
     this->operations_of_machine = instance.operations_of_machine;
     this->processing_time = instance.processing_time;
     this->num_objectives = 4;
-    this->senses = std::vector<BRKGA::Sense>(4, BRKGA::Sense::MINIMIZE);
+    this->senses = std::vector<BRKGA::Sense>(this->num_objectives, BRKGA::Sense::MINIMIZE);
     this->primal_bound = instance.primal_bound;
     return *this;
 }
@@ -243,6 +243,7 @@ std::istream & operator >>(std::istream & is, Instance & instance) {
                                           std::vector<std::pair<unsigned, unsigned>>());
     instance.processing_time.clear();
     instance.num_objectives = 4;
+    instance.senses = std::vector<BRKGA::Sense>(instance.num_objectives, BRKGA::Sense::MINIMIZE);
 
     for (unsigned job = 0; job < instance.num_jobs; job++) {
         is >> instance.num_operations[job];
